9c67f3b1713d53ce3197bcd8248edfad3abdc53f
kalpee <shogo.galleria.game@gmail.com>
2021-09-05 23:04:32 +0900

    フォームリクエスト

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
                     <form action="{{ route('owner.shops.update', ['shop' => $shop->id])}}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="-m-2">
@@ -20,7 +21,7 @@
                             </div>
                             <div class="p-2 w-full flex justify-around mt-4">
                                 <button type="button" onclick="location.href='{{ route('owner.shops.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                                <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新</button>                        
+                                <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>                        
                             </div>
                         </div>
                     </form>
