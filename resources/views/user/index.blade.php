cbd54dee179f2813897190d05c053a2c1396a0e1
kalpee <shogo.galleria.game@gmail.com>
2021-09-21 01:02:15 +0900

    表示件数

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -23,7 +23,7 @@
                                 <option value="{{ \Constant::SORT_ORDER['lowerPrice']}}"
                                     @if(\Request::get('sort') === \Constant::SORT_ORDER['lowerPrice'] )
                                     selected
-                                    @endif>料金の低い順
+                                    @endif>料金の安い順
                                 </option>
                                 <option value="{{ \Constant::SORT_ORDER['later']}}"
                                     @if(\Request::get('sort') === \Constant::SORT_ORDER['later'] )
@@ -37,7 +37,26 @@
                                 </option> 
                             </select>
                         </div>
-                        <div>表示件数</div>
+                        <div>
+                            <span class="text-sm">表示件数</span><br>
+                            <select id="pagination" name="pagination">
+                                <option value="20"
+                                    @if(\Request::get('pagination') === '20')
+                                    selected
+                                    @endif>20件
+                                </option>
+                                <option value="50"
+                                    @if(\Request::get('pagination') === '50')
+                                    selected
+                                    @endif>50件
+                                </option> 
+                                <option value="100"
+                                    @if(\Request::get('pagination') === '100')
+                                    selected
+                                    @endif>100件
+                                </option>  
+                            </select>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -49,21 +68,25 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-wrap">
-                            @foreach ($products as $product)
-                            <div class="w-1/4 p-2 md:p-4">
-                                <a href="{{ route('user.items.show', ['item' => $product->id ])}}">
-                                    <div class="border rounded-md p-4">
-                                        <x-thumbnail filename="{{ $product->filename ?? ''}}" type="products" />
-                                        <div class="mt-4">
-                                            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category }}</h3>
-                                            <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
-                                            <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円(税込)</span></p>
-                                        </div>
+                        @foreach ($products as $product)
+                        <div class="w-1/4 p-2 md:p-4">
+                            <a href="{{ route('user.items.show', ['item' => $product->id ])}}">
+                                <div class="border rounded-md p-4">
+                                    <x-thumbnail filename="{{ $product->filename ?? ''}}" type="products" />
+                                    <div class="mt-4">
+                                        <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category }}</h3>
+                                        <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
+                                        <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円(税込)</span></p>
                                     </div>
-                                </a>
-                            </div>
-                            @endforeach
+                                </div>
+                            </a>
                         </div>
+                        @endforeach
+                    </div>
+                    {{ $products->appends([
+                        'sort' => \Request::get('sort'),
+                        'pagination' => \Request::get('pagination')
+                    ])->links() }}
                 </div>
             </div>
         </div>
@@ -71,6 +94,10 @@
 <script>
     const select = document.getElementById('sort')
     select.addEventListener('change', function(){
+        this.form.submit()
+    })
+    const paginate = document.getElementById('pagination')
+    paginate.addEventListener('change', function(){
         this.form.submit()
     })
 </script>
